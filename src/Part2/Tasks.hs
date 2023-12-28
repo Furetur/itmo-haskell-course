26fa51c5a7c262c534b125689ce221e2953e79c6
Furetur <furetur@gmail.com>
2023-12-28 16:39:42 +0300

    Part2

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -1,29 +1,49 @@
 module Part2.Tasks where
 
-import Util(notImplementedYet)
+import Util (notImplementedYet)
 
 data BinaryOp = Plus | Minus | Times deriving (Show, Eq)
 
-data Term = IntConstant { intValue :: Int }          -- числовая константа
-          | Variable    { varName :: String }        -- переменная
-          | BinaryTerm  { op :: BinaryOp, lhv :: Term, rhv :: Term } -- бинарная операция
-             deriving(Show,Eq)
+data Term
+  = IntConstant {intValue :: Int} -- числовая константа
+  | Variable {varName :: String} -- переменная
+  | BinaryTerm {op :: BinaryOp, lhv :: Term, rhv :: Term} -- бинарная операция
+  deriving (Show, Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) = notImplementedYet
+(|+|) = BinaryTerm Plus
+
 (|-|) :: Term -> Term -> Term
-(|-|) = notImplementedYet
+(|-|)= BinaryTerm Minus
+
 (|*|) :: Term -> Term -> Term
-(|*|) = notImplementedYet
+(|*|) = BinaryTerm Times
+
+infixl 1 |+|
+infixl 1 |-|
+infixl 2 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement (Variable name) = if varName == name then replacement else Variable name
+replaceVar varName replacement (IntConstant i) = IntConstant i
+replaceVar varName replacement (BinaryTerm op l r) = BinaryTerm op (replaceVar varName replacement l) (replaceVar varName replacement r)
+
+calc :: BinaryOp -> Int -> Int -> Int
+calc Plus = (+)
+calc Minus = (-)
+calc Times = (*)
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate = notImplementedYet
+evaluate (BinaryTerm op l r) =
+  let l' = evaluate l
+      r' = evaluate r
+   in case (l', r') of
+        (IntConstant i1, IntConstant i2) -> IntConstant (calc op i1 i2)
+        _ -> BinaryTerm op l' r'
+evaluate x = x
