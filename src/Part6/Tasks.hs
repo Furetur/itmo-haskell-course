da95f423117cc8ac49303288744769907cfd9e44
Furetur <furetur@gmail.com>
2023-12-29 17:25:35 +0300

    Part 6: everything except determinant

--- src/Part6/Tasks.hs
+++ src/Part6/Tasks.hs
@@ -1,38 +1,88 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
 module Part6.Tasks where
 
-import Util (notImplementedYet)
+import qualified Data.List
 import Data.Map
+import Data.Maybe (fromMaybe)
+import Util (notImplementedYet)
+
+(|>) x f = f x
+
+outOfBounds () = error "Out Of Bounds"
+
+illegalDim () = error "Illegal dim"
 
 -- Разреженное представление матрицы. Все элементы, которых нет в sparseMatrixElements, считаются нулями
-data SparseMatrix a = SparseMatrix {
-                                sparseMatrixWidth :: Int,
-                                sparseMatrixHeight :: Int,
-                                sparseMatrixElements :: Map (Int, Int) a
-                         } deriving (Show, Eq)
+data SparseMatrix a = SparseMatrix
+  { sparseMatrixWidth :: Int,
+    sparseMatrixHeight :: Int,
+    sparseMatrixElements :: Map (Int, Int) a
+  }
+  deriving (Show, Eq)
+
+
+getElementFromSparse m (i, j) = fromMaybe 0 (Data.Map.lookup (i, j) m)
 
 -- Определите класс типов "Матрица" с необходимыми (как вам кажется) операциями,
 -- которые нужны, чтобы реализовать функции, представленные ниже
 class Matrix mx where
+  zero :: Int -> Int -> mx
+  eye :: Int -> mx
+  dim :: mx -> (Int, Int)
+  multiplyMatrix :: mx -> mx -> mx
 
 -- Определите экземпляры данного класса для:
 --  * числа (считается матрицей 1x1)
 --  * списка списков чисел
 --  * типа SparseMatrix, представленного выше
 instance Matrix Int where
-instance Matrix [[Int]] where
-instance Matrix (SparseMatrix Int) where
+  zero _ _ = 0
 
--- Реализуйте следующие функции
--- Единичная матрица
-eye :: Matrix m => Int -> m
-eye w = notImplementedYet
--- Матрица, заполненная нулями
-zero :: Matrix m => Int -> Int -> m
-zero w h = notImplementedYet
--- Перемножение матриц
-multiplyMatrix :: Matrix m => m -> m -> m
-multiplyMatrix = notImplementedYet
+  eye _ = 1
+
+  dim _ = (1, 1)
+
+  multiplyMatrix m1 m2 = m1 * m2
+
+instance Matrix [[Int]] where
+  zero w h = if w > 0 && h > 0 then replicate h (replicate w 0) else illegalDim ()
+
+  eye n = if n > 0 then Prelude.map eye_row [0 .. n - 1] else illegalDim ()
+    where
+      eye_row i = Prelude.map (\x -> if x == i then 1 else 0) [0 .. n - 1]
+
+  dim (row : rest) = (length row, length rest + 1)
+
+  multiplyMatrix m1 m2 =
+    let (x, _) = dim m1
+        (_, y) = dim m2
+     in if x == y
+          then [[sum $ zipWith (*) row1 y | y <- Data.List.transpose m2] | row1 <- m1]
+          else illegalDim ()
+
+instance Matrix (SparseMatrix Int) where
+  zero w h = SparseMatrix w h Data.Map.empty
+
+  eye n =
+    let els = Prelude.foldl (\acc i -> Data.Map.insert (i, i) 1 acc) Data.Map.empty [0 .. n - 1]
+     in SparseMatrix n n els
+
+  dim (SparseMatrix w h _) = (w, h)
+
+  multiplyMatrix :: SparseMatrix Int -> SparseMatrix Int -> SparseMatrix Int
+  multiplyMatrix (SparseMatrix w1 h1 m1) (SparseMatrix w2 h2 m2) =
+    if h2 == w1
+      then SparseMatrix w2 h1 m
+      else illegalDim ()
+    where
+      m = Data.Map.fromList (filterZero lst)
+      lst = [((i, j), rowByCol i j) | i <- [0..h1-1], j <- [0..w2-1]]
+      rowByCol i j = sum $ Prelude.map (\k -> getElementFromSparse m1 (i, k) * getElementFromSparse m2 (k, j)) [0..w1-1]
+      filterZero = Prelude.filter (\(_, x) -> x /= 0)
+
+
 -- Определитель матрицы
-determinant :: Matrix m => m -> Int
+determinant :: (Matrix m) => m -> Int
 determinant = notImplementedYet
--- src/Part6/Tests.hs
+++ src/Part6/Tests.hs
@@ -1,24 +1,72 @@
 module Part6.Tests where
 
 import qualified Data.Map
-
+import Data.Maybe (fromMaybe)
+import Part6.Tasks
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
-import Part6.Tasks
 
 unit_eye = do
-    eye 1 @?= one
-    eye 1 @?= [[one]]
-    eye 1 @?= SparseMatrix 1 1 (Data.Map.fromList [((0, 0), one)])
-    eye 2 @?= [[one, 0], [0, one]]
-    eye 2 @?= SparseMatrix 2 2 (Data.Map.fromList [((0, 0), one), ((1, 1), one)])
-
-    where one :: Int; one = 1
+  eye 1 @?= one
+  eye 1 @?= [[one]]
+  eye 1 @?= SparseMatrix 1 1 (Data.Map.fromList [((0, 0), one)])
+  eye 2 @?= [[one, 0], [0, one]]
+  eye 2 @?= SparseMatrix 2 2 (Data.Map.fromList [((0, 0), one), ((1, 1), one)])
+  where
+    one :: Int; one = 1
 
 unit_zero = do
-    zero 1 1 @?= zz
-    zero 2 1 @?= [[zz, zz]]
-    zero 2 2 @?= [[zz, zz], [zz, zz]]
-    zero 5 5 @?= SparseMatrix 5 5 (Data.Map.fromList ([]::[((Int, Int), Int)]))
-    where zz :: Int; zz = 0
+  zero 1 1 @?= zz
+  zero 2 1 @?= [[zz, zz]]
+  zero 2 2 @?= [[zz, zz], [zz, zz]]
+  zero 5 5 @?= SparseMatrix 5 5 (Data.Map.fromList ([] :: [((Int, Int), Int)]))
+  where
+    zz :: Int; zz = 0
 
+unit_multiplyMatrix_list = do
+  (z `multiplyMatrix` o) @?= z
+  (o `multiplyMatrix` z) @?= z
+  (o `multiplyMatrix` o) @?= o
+  where
+    z :: [[Int]]
+    z = zero d d
+    o :: [[Int]]
+    o = eye d
+    d = 100
+
+unit_multiplyMatrix_sparse = do
+  (z `multiplyMatrix` o) @?= z
+  (o `multiplyMatrix` z) @?= z
+  (o `multiplyMatrix` o) @?= o
+  where
+    z :: SparseMatrix Int
+    z = zero d d
+    o :: SparseMatrix Int
+    o = eye d
+    d = 100
+
+sparseToList (SparseMatrix w h els) =
+  let row_indices = [0 .. (h - 1)]
+      col_indices = [0 .. (w - 1)]
+      get_element row col = Data.Map.lookup (row, col) els |> fromMaybe 0
+      make_row i = Prelude.map (get_element i) col_indices
+   in Prelude.map make_row row_indices
+
+listToSparse matrix =
+  SparseMatrix
+    { sparseMatrixWidth = length (head matrix),
+      sparseMatrixHeight = length matrix,
+      sparseMatrixElements = Data.Map.fromList [((i, j), val) | (row, i) <- zip matrix [0 ..], (val, j) <- zip row [0 ..], val /= 0]
+    }
+
+diag list = [[if col_j == row_i then x else 0 | (_, col_j) <- zip list [0 ..]] | (x, row_i) <- zip list [0 ..]]
+
+someMatrix list = [[if col_j == row_i then x + row_i else col_j | (_, col_j) <- zip list [0 ..]] | (x, row_i) <- zip list [0 ..]]
+
+prop_multiplication_of_list_and_sparse_is_the_same lst =
+  listToSparse (a `multiplyMatrix` b) == listToSparse a `multiplyMatrix` listToSparse b
+  where
+    lst' = 1:lst
+    a :: [[Int]]
+    a = diag lst'
+    b = someMatrix lst'
