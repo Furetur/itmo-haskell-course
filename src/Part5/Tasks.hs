21110a40dc32a763182c086f79cd0dc5914c3957
Furetur <furetur@gmail.com>
2023-12-29 13:15:10 +0300

    Part 5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -1,32 +1,41 @@
 module Part5.Tasks where
 
-import Util(notImplementedYet)
+import Data.IntMap (partition)
+import Util (notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl f acc [] = acc
+myFoldl f acc (x:xs) = myFoldl f (f acc x) xs
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr f acc [] = acc
+myFoldr f acc (x:xs) = f x (myFoldr f acc xs)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldr helper []
+  where
+    helper el acc = f el : acc
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myConcat . map f
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldl (<>) []
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldl (flip (:)) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldr helper []
+  where
+    helper x acc = if p x then x : acc else acc
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
-
+myPartition p lst =
+  let satisfy = myFilter p lst
+      doNotSatisfy = myFilter (not . p) lst
+   in (satisfy, doNotSatisfy)
